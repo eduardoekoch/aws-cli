bf93367125b839181015d2ae70c36c78a4ed7b14
eduardoekoch <eduardo.ekoch@gmail.com>
2017-07-14 16:11:30 -0300

    Update RestoreRDS.sh

--- RestoreRDS.sh
+++ RestoreRDS.sh
@@ -7,7 +7,7 @@
 aws rds delete-db-instance --db-instance-identifier fin-stg --skip-final-snapshot
 
 #Aguardar que o Status seja igual a " "
-status='new'
+status='deleting'
 
 until [ status != 'deleting' ]
  do
@@ -19,6 +19,9 @@ until [ status != 'deleting' ]
 
 #Restaura o Snapshot
   aws rds restore-db-instance-from-db-snapshot --db-instance-identifier fin-stg --db-subnet-group-name bioritmo-sub-rds --publicly-accessible --db-snapshot-identifier arn:aws:rds:us-east-1:174928946679:snapshot:rds:fin --db-instance-class db.t2.small --no-multi-az
-  
+
+#Localiza o ARN do RDS criado
+  rds_stg=$aws rds describe-db-instances --query 'DBInstances[?DBInstanceIdentifier==`fin-stg`]'
+ 
 #Registra o RDS na Stack do Fin-Stg
-  
+  aws opsworks register-rds-db-instance --region us-east-1 --stack-id f77c5a79-b55d-48c3-b4cb-da2f6827d3a9 --rds-db-instance-arn fin_stg --db-user deploy --db-password 81haDHuRWj  
